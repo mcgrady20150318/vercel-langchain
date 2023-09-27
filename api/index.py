f4429501edfb0d4ae754274a91a61a40e368477d
cvpr2022-Anonymous <mcgrady150318@gmail.com>
2023-09-27 09:37:56 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,9 @@ from langchain.llms import OpenAI
 
 app = Flask(__name__)
 
+os.environ["OPENAI_API_KEY"] = "mk-WOb5xz6NB9NfXJM6REnzs4RGqcdYe0Q64hnbLulzPOEAXiP0"
+os.environ['OPENAI_API_BASE'] = 'https://api.aiproxy.io/v1'
+
 @app.route('/')
 def home():
     llm = OpenAI(temperature=0.9)
